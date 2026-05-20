1cea67eced9e2ebc39234e099a45df634ae3c647
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-05-20 17:04:22 +0530

    Refactor OOPS.java to improve Pen class methods and enhance encapsulation

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -3,45 +3,53 @@ public class OOPS {
   public static void main(String[] args) {
       Pen p1 = new Pen(); //created a pen object called p1
       p1.setColor("blue");
-      System.out.println(p1.color);
+      System.out.println(p1.getColor());
       p1.setTip(5);
-      System.out.println(p1.tip);
+      System.out.println(p1.getTip());
+      p1.setColor("Yellow");
+      System.out.println(p1.getColor());
 
-      BankAccount myAcc = new BankAccount();
-      myAcc.username = "Saquib";
-      myAcc.setPassword("2463d");;
+      
   }
 
 }
 
-class BankAccount {
-  public String username;
-  private String password;
-  public void setPassword(String pwd) {
-    password = pwd;
-  }
-}
+// class BankAccount {
+//   public String username;
+//   private String password;
+//   public void setPassword(String pwd) {
+//     password = pwd;
+//   }
+// }
 
 class Pen {
-  String color;
-  int tip;
+  private String color;
+  private int tip;
+
+  String getColor() {
+    return this.color;
+  }
+
+  int getTip() {
+    return this.tip;
+  }
 
   void setColor(String newColor) {
-    color = newColor;
+    this.color = newColor;
   }
 
-  void setTip(int newTip) {
-    tip = newTip;
-  }
-}
-
-class Student {
-  String name;
-  int age;
-  float percentage; //cgpa
-
-  void calcPercentage(int phy, int chem, int math) {
-    percentage = (phy + chem + math) / 3;
+  void setTip(int tip) {
+    this.tip = tip;
   }
 }
+
+// class Student {
+//   String name;
+//   int age;
+//   float percentage; //cgpa
+
+//   void calcPercentage(int phy, int chem, int math) {
+//     percentage = (phy + chem + math) / 3;
+//   }
+// }
 
