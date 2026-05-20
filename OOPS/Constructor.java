19d61e274d83a8c1d3ff64738b8f552b516c9118
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-05-20 17:17:27 +0530

    Add Constructor and Student classes with basic constructor implementations

---
+++ OOPS/Constructor.java
@@ -0,0 +1,30 @@
+public class Constructor {
+
+  public static void main(String[] args) {
+      Student s1 = new Student();
+      Student s2 = new Student("Saquib");
+      Student s3 = new Student(65);
+      
+
+  }
+}
+
+class Student {
+  String name;
+  int roll;
+
+    Student() {
+      System.out.println("Constructor is called...");
+    }
+    Student(String name) {
+      this.name = name;
+    }
+
+    Student(int roll) {
+      this.roll = roll;
+    }
+
+    
+
+  
+}
