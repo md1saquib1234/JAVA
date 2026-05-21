e952a2ae9767f3fb8337182fcfc701061aac9664
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-05-21 09:32:25 +0530

    Enhance Student class with copy constructor and update main method to demonstrate usage

--- OOPS/Constructor.java
+++ OOPS/Constructor.java
@@ -2,9 +2,23 @@ public class Constructor {
 
   public static void main(String[] args) {
       Student s1 = new Student();
-      Student s2 = new Student("Saquib");
-      Student s3 = new Student(65);
+      s1.name = "Saquib";
+      s1.roll = 456;
+      s1.password = "abc123";
+      s1.marks[0] = 90;
+      s1.marks[1] = 80;
+      s1.marks[2] = 70;
+      // Student s2 = new Student("Saquib");
+      // Student s3 = new Student(65);
+
+      Student s2 = new Student(s1);
+      s2.password = "xyz";
       
+
+      for (int i=0; i<3; i++) {
+        System.out.println(s2.marks[i]);
+      }
+
 
   }
 }
@@ -12,8 +26,19 @@ public class Constructor {
 class Student {
   String name;
   int roll;
+  String password;
+  int marks[];
+
+  //copy constructor
+  Student (Student s1) {
+    marks = new int[3];
+    this.name = s1.name;
+    this.roll = s1.roll;
+    this.marks = s1.marks;
+  }
 
     Student() {
+       marks = new int[3];
       System.out.println("Constructor is called...");
     }
     Student(String name) {
@@ -21,6 +46,7 @@ class Student {
     }
 
     Student(int roll) {
+       marks = new int[3];
       this.roll = roll;
     }
 
