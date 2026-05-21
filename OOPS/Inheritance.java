670315fdb56df0982fe9d943689004a069ec5419
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-05-21 10:17:09 +0530

    Add Bird and Dog classes; update Inheritance class to demonstrate functionality of new classes

--- OOPS/Inheritance.java
+++ OOPS/Inheritance.java
@@ -1,8 +1,19 @@
 public class Inheritance {
 
   public static void main(String[] args) {
-    Fish shark = new Fish();
-    shark.eat();
+    // Fish shark = new Fish();
+    // shark.eat();
+
+    Dog dobby = new Dog();
+    dobby.eat();
+    Bird parrot = new Bird();
+    parrot.fly();
+
+    Fish nemo = new Fish();
+    nemo.eat();
+    nemo.swin();
+    nemo.color = "Orange";
+    System.out.println("Color of nemo is " + nemo.color);
       
   }
 
@@ -12,6 +23,7 @@ public class Inheritance {
 
 class Animal {
   String color;
+
   void eat() {
     System.out.println("eats");
   }
@@ -23,10 +35,30 @@ class Animal {
 
 
 //Derived class
-class Fish extends Animal {
-  int fins;
+// class Fish extends Animal {
+//   int fins;
 
-  void swim() {
-    System.out.println("Swims in water");
+//   void swim() {
+//     System.out.println("Swims in water");
+//   }
+// }
+
+class Mammal extends Animal {
+  void walk() {
+    System.out.println("walks on land");
+  }
+
+
+}
+
+class Fish extends Animal {
+  void swin() {
+    System.out.println("swims in water");
+  }
+}
+
+class Bird extends Animal {
+  void fly() {
+    System.out.println("flies in the sky");
   }
 }
