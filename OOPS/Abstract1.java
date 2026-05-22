f5a5b60fc6334e8c7abfdbea5bc8d7f53444dc72
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-05-22 10:45:52 +0530

    Add Abstract1 class with ChessPlayer interface and implementations for Queen, Rook, and King

---
+++ OOPS/Abstract1.java
@@ -0,0 +1,33 @@
+public class Abstract1 {
+
+  public static void main(String[] args) {
+    Queen q = new Queen();
+    q.moves();
+
+
+  }
+
+
+}
+
+interface ChessPlayer {
+  void moves();
+}
+
+class Queen implements ChessPlayer {
+  public void moves() {
+    System.out.println("up, down, left, right, diagonal (in all 4 directions");
+  }
+}
+
+class Rook implements ChessPlayer {
+  public void moves() {
+    System.out.println("up, down, left, right");
+  }
+}
+
+class King implements ChessPlayer {
+  public void moves() {
+    System.out.println("up, down, left, right, diagonal - (by 1 step");
+  }
+}
