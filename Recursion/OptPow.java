f40ea7371b6d84b531278392d90aadfb2db89f99
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-03 12:11:09 +0530

    Add OptPow class with method for calculating optimized power of a number using recursion

---
+++ Recursion/OptPow.java
@@ -0,0 +1,23 @@
+public class OptPow {
+
+  public static int optimizedPower(int a, int n) {// TC = O(logn)
+    if (n == 0) {
+      return 1;
+    }
+    int halfPower = optimizedPower(a, n / 2);
+    int halfPowerSq = halfPower * halfPower;
+    //n is odd
+    if (n % 2 != 0) {
+      halfPowerSq = a * halfPowerSq;
+     }
+
+     return halfPowerSq;
+
+  }
+
+  public static void main(String[] args) {
+    int a = 2;
+    int n = 5;
+    System.out.println(optimizedPower(a, n));
+  }
+}
