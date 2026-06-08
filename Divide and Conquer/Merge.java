9af33e83a174251cf60ca4e2f1157bac0aca7afa
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-08 13:03:04 +0530

    Update Merge class to include a negative number in the test array for merge sort

--- Divide and Conquer/Merge.java
+++ Divide and Conquer/Merge.java
@@ -57,7 +57,7 @@ public class Merge{
   }
 
   public static void main(String[] args) {
-  int arr[] = {6, 3, 9, 5, 2, 8};
+  int arr[] = {6, 3, 9, 5, 2, 8, -2};
   mergeSort(arr, 0, arr.length - 1);
   printArray(arr);
 }
